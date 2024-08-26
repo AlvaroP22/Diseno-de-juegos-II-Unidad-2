f85ec91ed8d05a2874e6bd157151a776aafd55e8
AlvaroP22 <alvaro.pozos8662@alumnos.udg.mx>
2024-08-26 11:58:32 -0600

    2.1

---
+++ Assets/Menulinks.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menulinks : MonoBehaviour
+{
+    public void Abrirlink()
+    {
+        Application.OpenURL("https://www.udgvirtual.udg.mx/");
+    }
+}
