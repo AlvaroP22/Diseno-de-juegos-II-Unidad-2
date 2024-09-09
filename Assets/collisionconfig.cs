0cf41a56885cab5971dd7aa2320bc2283801c2e7
AlvaroP22 <alvaro.pozos8662@alumnos.udg.mx>
2024-09-09 09:49:35 -0600

    2.5

---
+++ Assets/collisionconfig.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionconfig : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        Debug.Log("tocando");
+    }
+}
+
